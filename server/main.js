17d05d216ca2a75b3eed0bef08f492396aa8ebe8
pdt1806 <nmphuc1806@gmail.com>
2023-11-20 10:00:48 -0800

    fix: :bug: dumb bug

--- server/main.js
+++ server/main.js
@@ -1,52 +1,47 @@
-import express from "express";
-import fetch from "node-fetch";
-import puppeteer from "puppeteer-core";
+import express from 'express';
+import fetch from 'node-fetch';
+import puppeteer from 'puppeteer';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ message: "Hello from server!" });
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello from server!' });
 });
 
-app.get("/smallcard/:id", async (req, res) => {
+app.get('/smallcard/:id', async (req, res) => {
   try {
     const { id } = req.params;
 
     fetch(`http://127.0.0.1:7000/user/${id}`)
       .then((response) => {
-        if (response == 404) {
-          res.status(404).send("User not found");
+        if (response.status == 404) {
+          res.status(404).send('User not found');
           return null;
         }
         return response.json();
       })
       .then(async (data) => {
         const browser = await puppeteer.launch({
-          headless: "new",
-          executablePath: "/usr/bin/chromium-browser",
-          args: [
-            "--no-sandbox",
-            "--disable-setuid-sandbox",
-            "--disable-gpu",
-            "--disable-software-rasterizer",
-          ],
+          headless: true,
+          executablePath: '/usr/bin/chromium-browser',
+          args: ['--no-sandbox', '--disable-setuid-sandbox'],
         });
         const page = await browser.newPage();
         await page.goto(
-          `http://localhost:5173/smallcard?username=${data["username"]}&avatar=${data["avatar_url"]}&status=${data["status"]}`,
-          { waitUntil: ["domcontentloaded", "load", "networkidle2"] }
+          `http://disi.bennynguyen.us/smallcard?username=${data['username']}&avatar=${data['avatar_url']}&status=${data['status']}`,
+          { waitUntil: ['domcontentloaded', 'load', 'networkidle2'] }
         );
         const screenshotBuffer = await page.screenshot({
           clip: { x: 0, y: 0, width: 1350, height: 450 },
         });
         await browser.close();
-        res.set("Content-Type", "image/png");
+        res.set('Content-Type', 'image/png');
         res.send(screenshotBuffer);
       });
   } catch (error) {
-    console.error("Error:", error);
-    res.status(500).send("Internal Server Error");
+    console.error('Error:', error);
+    res.status(500).send('Internal Server Error');
   }
 });
 
-app.listen(1911, () => console.log("Server is running on http://localhost:1911"));
+app.listen(1911, () => console.log('Server is running on http://localhost:1911'));
