5c44495b3ef62bf514bc52046c84d38b6f3933ad
Benny Nguyen <nmphuc1806@gmail.com>
2023-11-29 17:08:48 -0800

    perf: :art: better RWD

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import MainContent from '@/components/MainContent';
 import { isMobile } from '@/utils/tools';
-import { Box, Center, Text, Title } from '@mantine/core';
+import { Box, Center, ScrollArea, Text, Title } from '@mantine/core';
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import classes from './index.module.css';
@@ -13,7 +13,7 @@ const Home = () => {
   return (
     <Box
       w="100vw"
-      h={!isMobile ? '100vh' : '100%'}
+      h="100vh"
       display={'flex'}
       style={{
         background: "url('/images/background.png') center center fixed",
@@ -22,32 +22,34 @@ const Home = () => {
         backgroundRepeat: 'no-repeat',
       }}
     >
-      <Box className={classes.home}>
-        <Title size={!isMobile ? 60 : 40} ta={'center'} pt={isMobile ? 'xl' : '0'}>
-          Discord Status as Image
-        </Title>
-        <Box
-          mt="xl"
-          h="min-content"
-          p="xl"
-          maw="85%"
-          style={{ backgroundColor: '#00000050', borderRadius: !isMobile ? '25px' : '0' }}
-        >
-          <Center w="100%" h="100%">
-            <MainContent />
-          </Center>
-        </Box>
-        <Text mt="md" mb="md">
-          Created by{' '}
-          <Link
-            to="https://github.com/pdt1806"
-            target="_blank"
-            style={{ color: 'white', textDecoration: 'none', fontWeight: 'bold' }}
+      <ScrollArea w="100%">
+        <Box className={classes.home} mt="5vh">
+          <Title size={!isMobile ? 55 : 40} ta={'center'}>
+            Discord Status as Image
+          </Title>
+          <Box
+            mt="xl"
+            h="min-content"
+            p="xl"
+            maw="85%"
+            style={{ backgroundColor: '#00000050', borderRadius: !isMobile ? '25px' : '0' }}
           >
-            pdt1806
-          </Link>
-        </Text>
-      </Box>
+            <Center w="100%" h="100%">
+              <MainContent />
+            </Center>
+          </Box>
+          <Text mt="md" mb="md">
+            Created by{' '}
+            <Link
+              to="https://github.com/pdt1806"
+              target="_blank"
+              style={{ color: 'white', textDecoration: 'none', fontWeight: 'bold' }}
+            >
+              pdt1806
+            </Link>
+          </Text>
+        </Box>
+      </ScrollArea>
     </Box>
   );
 };
