c9aca92e2f9059bcab64b15796b197556e987939
pdt1806 <nmphuc1806@gmail.com>
2024-01-16 17:38:04 -0800

    perf: :zap: stop tracking env ts

--- src/env/env.ts
+++
@@ -1,9 +0,0 @@
-export const refinerAPI = {
-  prod: "https://refiner-api.bennynguyen.dev",
-  dev: "http://localhost:7000",
-}
-export const disiAPI = {
-  prod: "https://disi-api.bennynguyen.dev",
-  dev: "http://localhost:1911",
-}
-export const testing = false
