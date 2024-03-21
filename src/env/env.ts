83c4fea9a05e472248f9f154e007d818ea93bc93
pdt1806 <nmphuc1806@gmail.com>
2024-03-20 18:49:44 -0700

    build: :bug: track env

---
+++ src/env/env.ts
@@ -0,0 +1,13 @@
+export const refinerAPI = {
+  prod: "https://refiner-api.bennynguyen.dev",
+  dev: "http://localhost:7000",
+}
+export const disiAPI = {
+  prod: "https://disi-api.bennynguyen.dev",
+  dev: "http://localhost:1911",
+}
+export const web = {
+  prod: "https://disi.bennynguyen.dev",
+  dev: "http://localhost:5173",
+}
+export const testing = false
